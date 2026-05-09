722f6093785b20163c1d83a28cf1a5bff56923ef
GPCGamesbr <>
2026-05-08 21:42:20 -0300

    tarefa area retangulo feita

---
+++ README.md
@@ -0,0 +1,8 @@
+# Calcular área retângulo
+(x) Criar variáveis base, altura e área
+(x) Scanner para ler os dados de base e altura
+(x) Calcular área do retângulo
+(x) Mostrar área calculada
+---------------------------------------------------------------------------------
+
+# Calcular área triângulo
---
+++ areaRetangulo.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class areaRetangulo{
+    public static void main(String[] args) {
+
+    Scanner ler = new Scanner(System.in);
+    System.out.println("Digite a base: ");
+    double base = ler.nextDouble();
+    System.out.println("Digite a altura: ");
+    double altura = ler.nextDouble();
+
+    double area = base * altura;
+    System.out.printf("A área é: %.2f\n",area);
+    ler.close();
+
+    }
+}
