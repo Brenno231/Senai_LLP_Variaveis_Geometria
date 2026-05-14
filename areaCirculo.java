06aacdbcdd8853214b2895c83d63d0144a62b1d0
Brnn231 <brennosoares665@gmail.com>
2026-05-14 10:57:26 -0300

    correção areaCirculo args

--- areaCirculo.java
+++ areaCirculo.java
@@ -1,10 +1,10 @@
 public class areaCirculo {
     public static void main(String[] args) {
-        double raio = Double.parseDouble(args[0]);
         if (args.length == 0){ 
             System.out.println("Você não digitou nada, Digite algo por favor!!!");
             return;
     }
+    double raio = Double.parseDouble(args[0]);
     Double area = raio * raio * Math.PI;
     System.out.printf("Area do círculo é: %.2f",area);
 
