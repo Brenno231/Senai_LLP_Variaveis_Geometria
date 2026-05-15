6bc92be3da5a167936b70ab6f9a7220c383b841e
Brnn231 <brennosoares665@gmail.com>
2026-05-15 11:57:29 -0300

    Correção de indentação

--- areaBola.java
+++ areaBola.java
@@ -1,12 +1,12 @@
 public class areaBola {
     public static void main(String[] args) {
-        if (args.length == 0){
+        if (args.length == 0) {
             System.out.println("Digite o raio da Bola!!!!!!");
             return;
         }
         double areaDaBola, raio;
         raio = Double.parseDouble(args[0]);
         areaDaBola = 4 * Math.PI * raio * raio;
-        System.out.printf("A área da bola é: %.2f",areaDaBola);
+        System.out.printf("A área da bola é: %.2f", areaDaBola);
     }
 }
--- areaTubo.java
+++ areaTubo.java
@@ -1,6 +1,6 @@
 public class areaTubo {
     public static void main(String[] args) {
-        if(args.length < 2){
+        if (args.length < 2) {
             System.out.println("Digite respectivamente o raio do tubo é a altura dele!!!!!!!!!");
             return;
         }
@@ -8,6 +8,6 @@ public class areaTubo {
         raio = Double.parseDouble(args[0]);
         altura = Double.parseDouble(args[1]);
         areaDoTubo = 2 * Math.PI * raio * altura;
-        System.out.printf("A área do tubo é: %.2f\n",areaDoTubo);
+        System.out.printf("A área do tubo é: %.2f\n", areaDoTubo);
     }
 }
